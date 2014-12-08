f175a65052d3fa0c4c23936f85606992204d1586
Juhász Viktor <viktor.juhasz@balabit.com>
2014-12-08 12:56:17 +0100

    builder.py: Correct the configure opts
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

--- sbuild/builder.py
+++ sbuild/builder.py
@@ -1,8 +1,11 @@
 from BalabitBuilder import BalabitBuilder
 
 class GettextBuilder(BalabitBuilder):
-    def configure(self):
-        pass
+    def configure(self, configure_opts = None):
+        return True
+
+    def make(self, subcommand = None):
+        return True
 
 def get_builder():
     return GettextBuilder()
